36d7e0fe6a83dedc2fba5480a7af6add37a692cc
sharathkumar-dp <dpsharath.msr@gmail.com>
2023-03-25 16:28:08 +0530

    6th commit

--- main.tf
+++ main.tf
@@ -2,11 +2,17 @@ terraform {
   required_version = ">=0.12.24"
 }
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 resource "aws_cloudformation_stack" "bucket" {
   name = "s3-bucket-creation"
 
   template_body = file("templates/bucket.yml")
+}
+
+resource "aws_cloudformation_stack" "instance" {
+  name = "ec2-creation"
+
+  template_body = file("templates/instance.yml")
 }
